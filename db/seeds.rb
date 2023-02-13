bfedd7fec9ba1354fedb80da6f082924fb7b2cfe
KareemWilson <kareemmagdywilson@gmail.com>
2023-02-13 16:01:16 +0200

    Add commands in seeds.rb file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+
+
+# user = User.create!(name: "Ken")
+# food =Food.create!(name: "mash pizza", measurement_unit: 5, price: 5, user: user)
+# recipe = Recipe.create!(name: "Pizza", preparation_time: 20.10, cooking_time: 2, description: "here is des", public: true, user: user)
+# recipe_food1 = RecipeFood.create!(quantity: 5.0, recipe: recipe, food:food)
