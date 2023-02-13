2184702afc7fd0eb9a29242a251fd54b294023e0
KareemWilson <kareemmagdywilson@gmail.com>
2023-02-13 15:59:58 +0200

    Adjust datatype in migration files

--- db/migrate/20230213132457_create_food.rb
+++ db/migrate/20230213132457_create_food.rb
@@ -2,9 +2,9 @@ class CreateFood < ActiveRecord::Migration[7.0]
   def change
     create_table :foods do |t|
       t.string :name
-      t.decimal :measurement_unit
-      t.decimal :price
-      t.decimal :quantity
+      t.float :measurement_unit
+      t.float :price
+      t.float :quantity
       t.references :user, null: false, foreign_key: true
 
       t.timestamps
--- db/migrate/20230213132720_create_recipe_food.rb
+++ db/migrate/20230213132720_create_recipe_food.rb
@@ -1,7 +1,7 @@
 class CreateRecipeFood < ActiveRecord::Migration[7.0]
   def change
     create_table :recipe_foods do |t|
-      t.decimal :quantity
+      t.float :quantity
       t.references :recipe, null: false, foreign_key: true
       t.references :food, null: false, foreign_key: true
 
