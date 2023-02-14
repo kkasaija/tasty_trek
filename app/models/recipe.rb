1927bd102c4646d835cf91d2b20dc4e969c10d11
KareemWilson <kareemmagdywilson@gmail.com>
2023-02-14 11:13:10 +0200

    Fix linter Errors

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,4 +1,4 @@
 class Recipe < ApplicationRecord
-    belongs_to :user
-    has_many :recipe_food
+  belongs_to :user
+  has_many :recipe_food
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,4 @@
 class User < ApplicationRecord
-    has_many :recipes
-    has_many :foods
+  has_many :recipes
+  has_many :foods
 end
