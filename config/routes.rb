9269acfc1dd9f7408ee946c591ef7822d3d716c6
KareemWilson <kareemmagdywilson@gmail.com>
2023-02-14 16:41:35 +0200

    Add routes for recipes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,4 @@
 Rails.application.routes.draw do
   root 'home#index'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :recipes
 end
