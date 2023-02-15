f9993cfe15255198847026b2b8d64b09c3cf1551
KareemWilson <kareemmagdywilson@gmail.com>
2023-02-15 16:05:02 +0200

    Implement new recipe functionality

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -4,6 +4,15 @@ class RecipesController < ApplicationController
         @recipes = current_user.recipes
     end
 
+    def new
+        @recipe = Recipe.new
+    end
+
+    def create
+        ispublic = params[:public]
+        @recipe = Recipe.create!(recipe_params.merge(user: current_user))
+    end
+
     def show
         @recipe = Recipe.find(params[:id])
     end
@@ -13,5 +22,11 @@ class RecipesController < ApplicationController
         RecipeFood.where(recipe_id: @recipe.id).destroy_all
         @recipe.destroy!
         redirect_to recipes_path
+    end
+
+    private
+
+    def recipe_params
+        params.require(:recipe).permit(:name, :description, :preparation_time, :cooking_time, :public, :current_user)
     end
 end
