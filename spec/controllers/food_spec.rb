ead868358fe932f6fb9ef980eab8d59f0d2098f3
Kasaija Kenneth <kasaijak@gmail.com>
2023-02-15 18:25:03 +0300

    Add FoodController spec tests

---
+++ spec/controllers/food_spec.rb
@@ -0,0 +1,38 @@
+require 'rails_helper'
+
+RSpec.describe FoodController, type: :controller do
+  let(:user) { User.create(name: 'Kasaija Kenneth') }
+  let(:food) do
+    Food.create(name: 'Rolex',
+                measurement_unit: 'kilograms',
+                price: 45,
+                quantity: 4,
+                user: user)
+  end
+
+  describe '#index' do
+    it 'returns a successful response' do
+      get :index
+      expect(response).to be_successful
+    end
+  end
+
+  describe '#new' do
+    it 'returns a successful response' do
+      get :new
+      expect(response).to be_successful
+    end
+  end
+
+  describe '#create' do
+    it 'creates a new food' do
+      expect do
+        post :create, params: { food: { name: 'Rolex',
+                                        measurement_unit: 'kilograms',
+                                        price: 45,
+                                        quantity: 4,
+                                        user: user } }
+      end.to change(Food, :count).by(1)
+    end
+  end
+end
