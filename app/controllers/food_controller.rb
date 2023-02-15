b9730a8f4b2030db872a380e9ac232e1986e8abe
Kasaija Kenneth <kasaijak@gmail.com>
2023-02-15 13:53:06 +0300

    Add method to track current user

--- app/controllers/food_controller.rb
+++ app/controllers/food_controller.rb
@@ -6,4 +6,25 @@ class FoodController < ApplicationController
   def new 
 
   end
+
+  def create
+    @food = Food.create!(name: params[:name], measurement_unit: params[:measurement_unit], price: params[:price], quantity: params[:quantity], user: current_user)
+    
+    if @food.save
+      redirect_to food_index_path
+    else
+      render 'new'
+    end
+  end
+
+  def destroy
+    @food = Food.find(params[:id])
+    @food.destroy
+
+    redirect_to food_index_path
+  end
+
+  def food_params
+    params.require(:food).permit(:name, :measurement_unit, :price, :quantity)
+  end
 end
