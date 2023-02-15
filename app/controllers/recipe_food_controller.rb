f99bbb7a389cccee9ca796ded80c4ac832229e65
KareemWilson <kareemmagdywilson@gmail.com>
2023-02-15 16:02:03 +0200

    Create a recipe_food controller

---
+++ app/controllers/recipe_food_controller.rb
@@ -0,0 +1,8 @@
+class RecipeFoodController < ApplicationController
+    def destroy 
+        @ingredient = RecipeFood.find(params[:id])
+        @ingredient.destroy
+
+        redirect_to recipe_path(params[:recipe_id])
+    end
+end
