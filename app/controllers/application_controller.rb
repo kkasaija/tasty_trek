76d25e4c66df51159f70372fa12b4d7f538ef07b
Kasaija Kenneth <kasaijak@gmail.com>
2023-02-16 22:46:11 +0300

    Refactor Application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!, :configure_permitted_parameters, if: :devise_controller?
+  before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
   layout 'application'
 
   protected
