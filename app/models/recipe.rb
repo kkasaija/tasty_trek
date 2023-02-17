920aab0187d5c5b73ecb915dcd995ed49cbc10f0
KareemWilson <kareemmagdywilson@gmail.com>
2023-02-17 10:13:05 +0200

    Add options to recipe details

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,4 +1,5 @@
 class Recipe < ApplicationRecord
   belongs_to :user
-  has_many :recipe_food
+  has_many :recipe_food, dependent: :destroy
+  has_many :foods, through: :recipe_food, dependent: :destroy
 end
