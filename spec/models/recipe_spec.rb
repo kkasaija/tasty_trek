b71743814fe412bcb94ab4de23eab0933334b7b0
Kasaija Kenneth <kasaijak@gmail.com>
2023-02-17 17:05:25 +0300

    Add recipe model association tests

--- spec/models/recipe_spec.rb
+++ spec/models/recipe_spec.rb
@@ -33,4 +33,15 @@ RSpec.describe Recipe, type: :model do
     end
   end
 
+  context 'Testing Associations' do
+    it 'has_many recipes' do
+      assoc = Recipe.reflect_on_association(:recipe_food)
+      expect(assoc.macro).to eq :has_many
+    end
+
+    it 'belongs_to user' do
+      assoc = Recipe.reflect_on_association(:user)
+      expect(assoc.macro).to eq :belongs_to
+    end
+  end
 end
