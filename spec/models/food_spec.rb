5d64ea703aaa175e92bca8bdf0080627b23d6217
Kasaija Kenneth <kasaijak@gmail.com>
2023-02-17 15:36:20 +0300

    Add association spec tests for food model

--- spec/models/food_spec.rb
+++ spec/models/food_spec.rb
@@ -1,10 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Food, type: :model do
+
   before :each do
     @user = User.create!(name: 'Kasaija Kenneth', email: 'kasaijak@gmail.com', password: '123456', role: 'admin')
     @food = Food.new(name: 'Rolex', measurement_unit: 'kilograms', price: 45, quantity: 4, user: @user)
   end
+
   context 'Test Validations' do
     it 'is valid with valid attributes' do
       expect(@food).to be_valid
@@ -33,6 +35,17 @@ RSpec.describe Food, type: :model do
     it 'is not valid without user' do
       @food.user = nil
       expect(@food).to_not be_valid
+    end
+  end
+
+  context 'Test Associations' do
+    it 'has_many recipes' do
+      assoc = Food.reflect_on_association(:recipe_food)
+      expect(assoc.macro).to eq :has_many
+    end
+    it 'belongs_to user' do
+      assoc = Food.reflect_on_association(:user)
+      expect(assoc.macro).to eq :belongs_to
     end
   end
 end
